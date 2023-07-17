7d7143a24f51d17ae9c86897166aeafaa12e7f87
heroescovid <66176503+heroescovid@users.noreply.github.com>
2023-07-17 15:25:17 -0600

    said

--- python_flask/app.py
+++ python_flask/app.py
@@ -1,7 +1,11 @@
 from flask import Flask,render_template,url_for,request,jsonify
 from flask_cors import CORS
-import paho.mqtt as mqtt
+import paho.mqtt as mqtt,requests,time
 app = Flask(__name__)
 app.route("/")
 def index():
     return render_template("index.html")
+app.route("/api/datos")
+def datos():
+    return render_template("datos.html")
+app.run("0.0.0.0",port = 8897,debug = False)
